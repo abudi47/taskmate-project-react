e9ca549952ebc1349820747ecf4872d57771951f
Abudi <abduselamt47@gmail.com>
2024-09-28 08:34:58 -0700

    updated dhow task js and add task js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
-export const AddTask = () => {
+export const AddTask = ({tasklist, setTasklist}) => {
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    
+    const date =  new Date();
+    const newTask = {
+
+    }
+  }
+
+
   return (
-    <div>AddTask</div>
+    <section className='addTask'>
+      <form onSubmit={handlesubmit}>
+        <input type="text" name="task" autoComplete='off' placeholder='add task' maxLength="25"/>
+        <button type="submit"> Add</button>
+
+      </form>
+      
+    </section>
   )
 }
--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-export const ShowTask = () => {
+export const ShowTask = ({tasklist, setTasklist}) => {
+  
   return (
-    <div>ShowTask</div>
+    <section className="showTask"> 
+
+        <div className='head'>
+          <div>
+            <span className='title'>Todo</span>
+            <span className='count'>{tasklist.length}</span>
+
+          </div>
+          <button className='clearAll'> clear All</button>
+
+        </div>
+        <ul>
+          {tasks.map((task) => (
+            <li>
+            <p>
+              <span className='name'>{task.name}</span>
+              <span className='time'> {task.time}</span>
+            </p>
+            <i class="bi bi-pencil-square"></i>
+            <i class="bi bi-trash"></i>
+          </li>
+
+          ))}
+           
+          
+        </ul>
+    </section >
   )
 }
