63fd23af16c2b4b6c05571f2e4d065b437226cfd
Abudi <abduselamt47@gmail.com>
2024-09-29 05:42:12 -0700

    update edit and delete functionality added

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-export const AddTask = ({tasklist, setTasklist}) => {
+export const AddTask = ({tasklist, setTasklist , task ,  setTask}) => {
   const handlesubmit = (e) => {
     e.preventDefault();
 
+    if (task.id) {
+      const date = new Date();
+      const updatedTask = tasklist.map((todo) => (
+        todo.id === task.id ? {id: task.id, name: task.name, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}` } :todo
+      ))
+      setTasklist(updatedTask);
+      setTask({});
+
+    }
+    else {
     const date =  new Date();
     const newTask = {
       id: date.getTime(),
@@ -12,14 +22,15 @@ export const AddTask = ({tasklist, setTasklist}) => {
 
     }
     setTasklist([...tasklist , newTask]);
-    e.target.task.value = "";
+    setTask({});
   }
+}
 
 
   return (
     <section className='addTask'>
       <form onSubmit={handlesubmit}>
-        <input type="text" name="task" autoComplete='off' placeholder='add task' maxLength="25"/>
+        <input type="text" name="task" value={task.name || ""} autoComplete='off' placeholder='add task' maxLength="25" onChange={e => setTask({...task , name : e.target.value})}/>
         <button type="submit"> Add</button>
 
       </form>
--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-export const ShowTask = ({tasklist, setTasklist}) => {
-  
+export const ShowTask = ({tasklist, setTasklist , task , setTask}) => {
+  const handledit = (id) => {
+    const selectedTask = tasklist.find(todo => todo.id === id)
+    setTask(selectedTask);
+    // const updatedTasklist = 
+
+
+  }
+
+  const handedelete = (id) => {
+    const updatedTasklist = tasklist.filter(todo => todo.id !== id);
+    setTasklist(updatedTasklist);
+  }
   return (
     <section className="showTask"> 
 
@@ -9,20 +20,20 @@ export const ShowTask = ({tasklist, setTasklist}) => {
           <div>
             <span className='title'>Todo</span>
             <span className='count'>{tasklist.length}</span>
-
+;
           </div>
-          <button className='clearAll' onClick={() => setTasklist([])}> clear All</button>
+          <button onClick={() => setTasklist([])} className='clearAll' > clear All</button>
 
         </div>
         <ul>
-          {tasklist.map((task) => (
-            <li key={task.id}>
+          {tasklist.map((todo) => (
+            <li key={todo.id}>
             <p>
-              <span className='name'>{task.name}</span>
-              <span className='time'> {task.time}</span>
+              <span className='name'>{todo.name}</span>
+              <span className='time'> {todo.time}</span>
             </p>
-            <i class="bi bi-pencil-square"></i>
-            <i class="bi bi-trash"></i>
+            <i onClick={() => handledit(todo.id)} className="bi bi-pencil-square"></i>
+            <i onClick={() => handedelete(todo.id)}  className="bi bi-trash"></i>
           </li>
 
           ))}
