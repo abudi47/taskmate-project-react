c237148b688ee56c6bac1d6fe64901890a93df4c
Abduselam <136098639+abudi47@users.noreply.github.com>
2024-10-14 03:45:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { AddTask } from './components/AddTask';
 import { Header } from './components/Header';
